e3afc6970bbcfc81ef97706e3931abd7ba7533ba
Tran Luong <tran.huu.luong@sun-asterisk.com>
2022-06-13 13:59:27 +0700

    add github action

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_config/flutter_config.dart';
@@ -8,6 +9,7 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/screens/detail/detail_screen.dart';
 import 'package:movie_app/screens/main/main_screen.dart';
+import 'package:movie_app/screens/post/post_screen.dart';
 
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
     'high_importance_channel', // id
@@ -17,10 +19,12 @@ const AndroidNotificationChannel channel = AndroidNotificationChannel(
     playSound: true);
 
 final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+ final _navigationKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  initDynamicLinks();
 
   await FlutterConfig.loadEnvVariables();
 
@@ -57,6 +61,35 @@ void main() async {
   );
 
   runApp(const ProviderScope(child: MyApp()));
+}
+
+void initDynamicLinks() async {
+  final data = await FirebaseDynamicLinks.instance.getInitialLink();
+  final deepLink = data?.link;
+
+  if (deepLink != null) {
+    handleDynamicLink(deepLink);
+  }
+  FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) {
+    final deepLink = dynamicLinkData.link;
+
+    if (deepLink != null) {
+      // Hàm này để xử lý khi có Dynamic link gọi tới.
+      handleDynamicLink(deepLink);
+    }
+  }).onError((error) {});
+}
+
+// Ở đây nếu link có chứa "post" thì sẽ cho nhảy đến màn hình Post và truyền param thứ 2 của link là 56 qua màn hình Post.
+void handleDynamicLink(Uri url) {
+  List<String> separatedString = [];
+  separatedString.addAll(url.path.split('/'));
+  if (separatedString[1] == "post") {
+     Navigator.push(
+          _navigationKey.currentState!.context,
+          MaterialPageRoute(
+              builder: (context) => PostScreen(number: separatedString[2])));
+  }
 }
 
 class MyApp extends StatefulWidget {
@@ -115,6 +148,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: _navigationKey,
       home: const MainScreen(),
       routes: {DetailScreen.routeName: (context) => const DetailScreen()},
     );
